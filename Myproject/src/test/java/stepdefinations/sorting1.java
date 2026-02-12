00ceefdec637bc8de5cd5239f12bea175ffa4fae
KandrathiVijayalakshmi <vijayalakshmikandrathi1122@gmail.com>
2026-02-12 08:04:59 +0000

    Add files via upload

--- Myproject/src/test/java/stepdefinations/sorting1.java
+++ Myproject/src/test/java/stepdefinations/sorting1.java
@@ -16,65 +16,67 @@ public class sorting1 {
 WebDriver driver;
 @Given("browser is in open")
 public void browser_is_in_open() throws InterruptedException {
-	WebDriver driver = new ChromeDriver();
+    driver = new ChromeDriver();
 	driver.navigate().to("https://www.amazon.in");
 	Thread.sleep(1000);
-	driver.findElement(By.xpath("/html/body/div[1]/header/div/div[6]/div[2]/div/div/ul/li[7]/div/a")).click();//customer service
-	//driver.findElement(By.xpath("/html/body/div/div[1]/div[3]/div/div/form/div/div/span/span/button")).click();//continue option
-	//driver.findElement(By.xpath("/html/body/div[1]/header/div/div[5]/div[2]/div[2]/div/ul/li[1]/div/a")).click();//fresh opt
-	// WebElement obj=driver.findElement(By.xpath("/html/body/div[1]/header/div/div[1]/div[2]/div/form/div[2]/div[1]/input"));
-	//Thread.sleep(1000); 
-	//obj.sendKeys("phones" + Keys.ENTER); 
-	//Thread.sleep(2000);
-	
-	
-	
-	driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[5]/div/div[2]/div/div/form/div/div[1]/div/input")).click();
-	
-	//WebElement obj1= driver.findElement(By.xpath("/html/body/div[1]/header/div/div[7]/div/div[2]/div[2]/div[5]/div/a/span"));
-	Thread.sleep(1000);
-	//obj1.sendKeys("return policy" + Keys.ENTER);
-	driver.findElement(By.xpath("/html/body/div[1]/div[1]/article/div/div[2]/div/div/div[1]/ul/li[2]/a")).click();
-	Thread.sleep(1000);
-	driver.findElement(By.xpath("").click();
-	
-	
-}
-	
+}		
 
 @Given("go to sorting option and dropdown")
 public void go_to_sorting_option_and_dropdown() throws InterruptedException {
-	
-	System.out.println("sorting success");
+	driver.findElement(By.xpath("/html/body/div[1]/header/div/div[1]/div[3]/div/a[1]")).click();//orders&return
+	Thread.sleep(1000);
+	driver.navigate().back();
+	Thread.sleep(1000);
+	//System.out.println("sorting success");
     // Write code here that turns the phrase above into concrete actions
     //throw new io.cucumber.java.PendingException();
 }
-//WebElement drpdwn;
 @When("select low to high option")
 public void select_low_to_high_option() throws InterruptedException {
-     
-	driver.findElement(By.id("nav-hamburger-menu")).click();//all option
 	
+	driver.findElement(By.xpath("/html/body/div[1]/header/div/div[5]/div[2]/div[2]/div/ul/li[1]/div/a")).click();//fresh opt
 	
-	
-	//driver.findElement(By.xpath("/html/body/div[1]/header/div/div[5]/div[2]/div[2]/div/ul/li[5]/div/a")).click();
-	Thread.sleep(2000);
-	
-	
-	/*Select sel= new Select(drpdwn);
-	driver.findElement(By.className("a-dropdown-link")).click();
-	Thread.sleep(2000);
-	sel.selectByVisibleText("Price: Low to High");
-	Thread.sleep(1000);*/
 
-    // Write code here that turns the phrase above into concrete actions
-    //throw new io.cucumber.java.PendingException();
+	Thread.sleep(1000); 
+      driver.navigate().back();
+     Thread.sleep(1000); 
+	
 }
 
 @Then("back to search")
-public void back_to_search() {
+public void back_to_search() throws InterruptedException {
+	driver.findElement(By.xpath("/html/body/div[1]/header/div/div[5]/div[2]/div[2]/div/ul/li[6]/div/a")).click();//mobiles opt
+	Thread.sleep(2000); 
+	
+	driver.findElement(By.xpath("/html/body/div[1]/header/div/div[6]/div[1]/ul/li[6]/div/a/span")).click();
+	Thread.sleep(2000);//camera opt
+	
+	
+	driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/ul/li/span/a/div/label/i")).click();
+	Thread.sleep(2000);//amazon check box
+	
+	driver.navigate().refresh();
+	
+	driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div/span[1]/div[1]/div[2]/div/div/span/div/div/div[3]/div[5]/div/div/div/div[2]/div/form/div/div/span/div/span/span/button")).click();
+	Thread.sleep(2000);//add to cart1
+	
+	//driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div/span[1]/div[1]/div[4]/div/div/span/div/div/div[3]/div[5]/div/div/div/div[2]/div/form/div/div/span/div/span/span/button")).click();
+	//Thread.sleep(2000);//add to cart2
+	
+	driver.findElement(By.id("a-autoid-3-announce")).click();
+	Thread.sleep(2000);//add to cart 2
+	
+	//driver.findElement(By.xpath("/html/body/div[1]/header/div/div[1]/div[3]/div/a[2]")).click();
+	//Thread.sleep(2000);//goto cart
+	
+	driver.findElement(By.id("nav-cart")).click();
+	Thread.sleep(2000);//goto cart
+
+	driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[3]/div[5]/div/div[2]/div[1]/div/form/ul/div[3]/div[4]/div/div[2]/ul/div[2]/li[3]/span/span/div/label/i")).click();
+Thread.sleep(2000);//check box gift
 	System.out.println("back to sorting search");
-    // Write code here that turns the phrase above into concrete actions
+	driver.quit()
+;    // Write code here that turns the phrase above into concrete actions
     //throw new io.cucumber.java.PendingException();
 }
 
--- Myproject/src/test/resources/features/sortiing1.feature
+++ Myproject/src/test/resources/features/sortiing1.feature
@@ -5,5 +5,3 @@ Given browser is in open
 And  go to sorting option and dropdown 
 When  select low to high option 
 Then  back to search 
- 
-
